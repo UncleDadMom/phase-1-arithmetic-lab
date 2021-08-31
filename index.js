170c500b503c6afdde0b30ea3bd14d5db3298ce4
Patrick Wu <Ptrckwu@yahoo.com>
2021-08-31 11:08:53 -0400

    commit tree 7693c7e4b6029289354e1a32f33edc847247a832

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+const max = Math.max(5, 10, 20)
+const random = Math.floor(Math.random() + 1)
+let num3 = 20
+let num4 = 8
+const mod = num3 % num4
